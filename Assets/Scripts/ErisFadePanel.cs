e8fa34bc8eb24642bb0b1c17a07da91ed2bce25e
Bharath serman R <bhar22005.cd@rmkec.ac.in>
2025-11-18 19:39:27 +0530

    Way_Recent

---
+++ Assets/Scripts/ErisFadePanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ErisFadePanel : MonoBehaviour
+{
+    public Animator ErisFadeAnimator;
+    [Header("TimeLine")]
+    public PlayableDirector GlitchLevelTimeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ErisFadeAnimator.SetTrigger("Eris");
+
+        //Play the Timeline.
+        GlitchLevelTimeline.Play();
+    }
+}
